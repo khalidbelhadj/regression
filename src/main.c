e5d2222321f0ddf7fce73d29d92a1cb255bcc3e0
Khalid <Khalidbelhadj124@gmail.com>
2024-05-20 11:15:14 +0100

    Add stats rendering

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <assert.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,9 +9,14 @@
 #define WIDTH 800
 #define HEIGHT 800
 #define MARGIN 20
+#define AXIS_COLOR                                                             \
+    CLITERAL(Color) { 0, 0, 0, 255 }
 
-#define DRAND_MIN -10
-#define DRAND_MAX 10
+#define LINE_COLOR BLUE
+
+#define DRAND_MIN 0
+#define DRAND_MAX 2
+#define DRAND_RANGE (DRAND_MAX - DRAND_MIN)
 
 #define STEP_SIZE 0.01
 
@@ -61,8 +67,8 @@ Regression regression_new(double *xs, double *ys, int N) {
         .N = N,
         .xs = xs,
         .ys = ys,
-        .w = -randd(),
-        .b = -randd(),
+        .w = DRAND_MIN + DRAND_RANGE * (double)rand() / (double)RAND_MAX,
+        .b = 0.0,
         .min = (Vector2){INFINITY, INFINITY},
         .max = (Vector2){-INFINITY, -INFINITY},
     };
@@ -92,21 +98,21 @@ void regression_step(Regression *r) {
 void draw_axis(void) {
     // X axis
     DrawLineEx((Vector2){MARGIN - 2, HEIGHT - MARGIN},
-               (Vector2){WIDTH - MARGIN, HEIGHT - MARGIN}, 3.0, BLACK);
+               (Vector2){WIDTH - MARGIN, HEIGHT - MARGIN}, 3.0, AXIS_COLOR);
     Vector2 v1 = (Vector2){WIDTH - MARGIN + 2, HEIGHT - MARGIN};
     Vector2 v2 = (Vector2){v1.x - 8.66, v1.y + 5};
     Vector2 v3 = (Vector2){v1.x - 8.66, v1.y - 5};
-    DrawText("x", v1.x - 10, v1.y - 20, 12, BLACK);
-    DrawTriangle(v3, v2, v1, BLACK);
+    DrawText("x", v1.x - 10, v1.y - 20, 12, AXIS_COLOR);
+    DrawTriangle(v3, v2, v1, AXIS_COLOR);
 
     // Y axis
     DrawLineEx((Vector2){MARGIN, MARGIN}, (Vector2){MARGIN, HEIGHT - MARGIN},
-               3.0, BLACK);
+               3.0, AXIS_COLOR);
     v1 = (Vector2){MARGIN, MARGIN - 2};
     v2 = (Vector2){v1.x - 5, v1.y + 8.66};
     v3 = (Vector2){v1.x + 5, v1.y + 8.66};
-    DrawText("y", v1.x + 10, v1.y, 12, BLACK);
-    DrawTriangle(v1, v2, v3, BLACK);
+    DrawText("y", v1.x + 10, v1.y, 12, AXIS_COLOR);
+    DrawTriangle(v1, v2, v3, AXIS_COLOR);
 }
 
 Vector2 get_point_pos(double x, double y, Regression r) {
@@ -122,7 +128,7 @@ Vector2 get_point_pos(double x, double y, Regression r) {
 void draw_points(Regression r) {
     for (size_t i = 0; i < r.N; ++i) {
         Vector2 pos = get_point_pos(r.xs[i], r.ys[i], r);
-        DrawCircle(pos.x, pos.y, 5.0, RED);
+        DrawCircle(pos.x, pos.y, 4.0, RED);
     }
 }
 
@@ -149,7 +155,23 @@ void draw_mse_squares(Regression r) {
 
 void draw_line(Regression r) {
     DrawLineEx(get_point_pos(r.min.x, r.w * r.min.x + r.b, r),
-               get_point_pos(r.max.x, r.w * r.max.x + r.b, r), 3.0, BLUE);
+               get_point_pos(r.max.x, r.w * r.max.x + r.b, r), 3.0, LINE_COLOR);
+}
+
+void draw_stats(Regression r) {
+    Rectangle rect = (Rectangle){
+        WIDTH - 2 * MARGIN - 75,
+        HEIGHT - 2 * MARGIN - 75,
+        75.0f,
+        75.0f
+    };
+    DrawRectangleRounded(rect, 0.2, 100, (Color){120, 120, 120, 120});
+
+    BeginScissorMode(rect.x, rect.y, rect.width, rect.height);
+    DrawText(TextFormat("w: %.2f", r.w),            5 + rect.x, 5 + rect.y + 20 * 0, 14, BLACK);
+    DrawText(TextFormat("b: %.2f", r.b),            5 + rect.x, 5 + rect.y + 20 * 1, 14, BLACK);
+    DrawText(TextFormat("loss: %.2f", mse_loss(r)), 5 + rect.x, 5 + rect.y + 20 * 2, 14, BLACK);
+    EndScissorMode();
 }
 
 int main(void) {
@@ -159,23 +181,39 @@ int main(void) {
     double ys[] = {1.0, 2.0, 3.0, 3.1, 0.9, 10, 9.2, 5.7, 4.3, 8.2};
 
     Regression r = regression_new(xs, ys, sizeof(xs) / sizeof(double));
-    printf("max: %f, %f\n", r.max.x, r.max.y);
-    printf("min: %f, %f\n", r.min.x, r.min.y);
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(WIDTH, HEIGHT, "window");
+    InitWindow(WIDTH, HEIGHT, "Regression");
     SetTargetFPS(60);
+
+    bool started = false;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(WHITE);
+        {
+            ClearBackground(WHITE);
 
-        regression_step(&r);
-        draw_axis();
-        draw_line(r);
-        draw_points(r);
-        // draw_mse_lines(r);
-        draw_mse_squares(r);
+            draw_axis();
+            draw_line(r);
+            draw_points(r);
+            draw_mse_squares(r);
+            draw_mse_lines(r);
+            draw_stats(r);
+
+            // Start screen
+            if (!started) {
+                DrawRectangle(0, 0, WIDTH, HEIGHT, (Color){0, 0, 0, 200});
+                const char *text = "Press SPACE to begin training";
+                unsigned int len = MeasureText(text, 20);
+                DrawText(text, WIDTH / 2 - len / 2, HEIGHT / 2 - 10, 20, WHITE);
+
+                if (GetKeyPressed() == KEY_SPACE)
+                    started = true;
+            } else {
+                regression_step(&r);
+            }
+
+        }
         EndDrawing();
     }
 
